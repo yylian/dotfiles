9868e69637fb3f81e5975559702d66c10a818323
hitimjj <julian.jarminowski@5minds.de>
2019-04-23 08:39:49 +0200

    Rename file

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ then
 fi
 
 bash scripts/01_install_programms.sh
-bash scripts/02_move-files.sh
+bash scripts/02_link-files.sh
 bash scripts/03_configure_applications.sh
 bash scripts/04_set_preferences.sh
 bash scripts/05_generate_ssh_key.sh
