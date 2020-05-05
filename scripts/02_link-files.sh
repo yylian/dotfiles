5375378d5819ef42681588c281cdf87cd41552c4
yylian <julian@jarminowski.de>
2020-05-05 22:41:12 +0200

    Add ssh config

--- scripts/02_link-files.sh
+++ scripts/02_link-files.sh
@@ -1,8 +1,10 @@
 mkdir ~/dev
 mkdir ~/Virtuelle_Maschinen
+mkdir ~/.ssh
 
 touch ~/.hushlogin
 
 ln -sf assets/.gitignore ~/.
 ln -sf assets/.bash-aliases ~/.
 ln -sf assets/.zshrc ~/.
+ln -sf assets/ssh_config ~/.ssh/config
