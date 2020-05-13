6ca597dbb4648da2192f5c1d203e02e89f303274
yylian <julian@jarminowski.de>
2020-05-13 22:21:00 +0200

    Update

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ then
     exit 1
 fi
 
+REPO_NAME="${REPO_NAME:.files}"
+CURRENT_PATH=$(pwd)
+DOTFILES_PATH="${CURRENT_PATH}/${REPO_NAME}"
 
-echo $(pwd)
-echo $REPO_NAME
-echo "ll"
 
 bash scripts/01_install_programms.sh
 bash scripts/02_link-files.sh
@@ -17,7 +17,8 @@ bash scripts/03_configure_applications.sh
 bash scripts/04_set_preferences.sh
 bash scripts/05_generate_ssh_key.sh
 
-open assets/finish.txt
+open "${DOTFILES_PATH}/finish.md"
+
 
 # z
 source /usr/local/etc/profile.d/z.sh
