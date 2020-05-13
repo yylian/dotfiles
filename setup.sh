d420d8502688193f2d65a2d79eacc278b5278070
yylian <julian@jarminowski.de>
2020-05-13 21:15:44 +0200

    Remove log

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-echo 'Installing brew'
-
 if test ! $(which brew); then
     echo "Installing homebrew"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
