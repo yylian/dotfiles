88f50db58869561e55b7e38c8fc09f13364b6b88
yylian <julian@jarminowski.de>
2020-11-19 22:31:15 +0100

    Add missing symlink

--- scripts/link_files.sh
+++ scripts/link_files.sh
@@ -9,7 +9,6 @@ mkdir ~/bin
 touch ~/.hushlogin
 
 ln -sf "${SYMLINK_DIRECTORY}/.gitignore" "~/."
-ln -sf "${SYMLINK_DIRECTORY}/.bash-aliases" "~/."
 ln -sf "${SYMLINK_DIRECTORY}/.tmux.conf" "~/."
 ln -sf "${SYMLINK_DIRECTORY}/.zshrc" "~/."
 ln -sf "${SYMLINK_DIRECTORY}/ssh_config" "~/.ssh/config"
