cdd95a4f0857f5f5a34d0a5632de5082ef03add4
yylian <julian@jarminowski.de>
2020-12-09 18:22:54 +0100

    Make alacritty config depending on os

--- scripts/link_files.sh
+++ scripts/link_files.sh
@@ -7,5 +7,10 @@ mkdir ~/.ssh
 ln -sf "${SYMLINK_DIRECTORY}/.gitignore" $HOME
 ln -sf "${SYMLINK_DIRECTORY}/.tmux.conf" $HOME
 ln -sf "${SYMLINK_DIRECTORY}/.zshrc" $HOME
-ln -sf "${SYMLINK_DIRECTORY}/.alacritty.yml" $HOME
 ln -sf "${SYMLINK_DIRECTORY}/ssh_config" "$HOME/.ssh/config"
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        ln -sf "${SYMLINK_DIRECTORY}/.alacritty.linux-gnu.yml" "${HOME}/.alacritty.yml"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        ln -sf "${SYMLINK_DIRECTORY}/.alacritty.darwin.yml" "${HOME}/.alacritty.yml"
+fi
