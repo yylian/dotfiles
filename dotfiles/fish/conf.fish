b8be5f457fbde319b98136d93915e226cc30ca23
yylian <julian@jarminowski.de>
2022-09-18 15:00:45 +0200

    Add backup alias

--- dotfiles/fish/conf.fish
+++ dotfiles/fish/conf.fish
@@ -39,6 +39,9 @@ pyenv virtualenv-init - | source
 # mclist
 function mclist_ip; echo "2a01:04f8:0252:1ce6::$argv:0"; end
 
+# local backup
+function backup; rsync --verbose --recursive --delete-before --whole-file --times --exclude=".DS_Store" --exclude=".Trash/" ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/ pi@solar:~/backup; end
+
 # Container
 #alias docker=podman
 #export DOCKER_HOST='unix:///Users/yylian/.local/share/containers/podman/machine/podman-machine-default/podman.sock'
