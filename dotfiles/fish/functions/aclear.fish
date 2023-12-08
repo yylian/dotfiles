39c439a2871b531ae0a2c0ff81d4782fa28a728f
yylian <julian@jarminowski.de>
2023-12-08 23:25:13 +0100

    Update scripts

--- dotfiles/fish/functions/aclear.fish
+++ dotfiles/fish/functions/aclear.fish
@@ -7,6 +7,9 @@ function aclear
     set -e KEYCLOAK_CLIENT_SECRET
     set -e KEYCLOAK_CLIENT_ID
     set -e KEYCLOAK_REALM
+    set -e GRAFANA_CLOUD_API_KEY
+    set -e TF_VAR_ghcr_password
+    set -e TF_VAR_keycloak_admin_password
 
     set -e ARM_PROD
     set -e ARM_STAGE
---
+++ dotfiles/fish/functions/unset_arm_client.fish
@@ -0,0 +1,5 @@
+function unset_arm_client
+    set -e ARM_CLIENT_ID
+    set -e ARM_CLIENT_CERTIFICATE_PATH
+    set -e ARM_CLIENT_CERTIFICATE_PASSWORD
+end
